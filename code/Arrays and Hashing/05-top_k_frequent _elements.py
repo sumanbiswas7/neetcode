4b48ee39ec9328ec82c2ae10c391c12839feebe6
suman <ronaldosuman97@gmail.com>
2022-05-19 21:35:48 +0530

    array-hashing p-5 added

---
+++ code/Arrays and Hashing/05-top_k_frequent _elements.py
@@ -0,0 +1,21 @@
+# MEDIUM
+
+# Given an integer array nums and an integer k, return the k most frequent elements.
+# You may return the answer in any order.
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        count = {}
+        freq = [[] for i in range(len(nums)+1)]
+        for c in nums:
+            count[c] = 1 + count.get(c , 0)
+        for n , c in count.items():
+            freq[c].append(n)
+        
+        res = []
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                res.append(n)
+                if k == len(res):
+                    return res
+    
