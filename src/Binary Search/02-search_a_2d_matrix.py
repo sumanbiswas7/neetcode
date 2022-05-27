6742256ecc2d36e99fcf7182dad397922f354e54
suman <ronaldosuman97@gmail.com>
2022-05-27 10:15:39 +0530

    binary search p-2 -a

--- src/Binary Search/02-search_a_2d_matrix.py
+++ src/Binary Search/02-search_a_2d_matrix.py
@@ -5,18 +5,32 @@
 # The first integer of each row is greater than the last integer of the previous row.
 
 def searchMatrix( matrix, target):
-    row , col = len(matrix), len(matrix[0])
-    top , bottom = 0, row-1
+    top , bottom = 0 , len(matrix)-1
 
     while top <= bottom:
-        mid = (top + bottom) //2
-
-        if target > matrix[mid][-1]:
-            top = mid + 1
-        elif target < matrix[mid][0]:
+        mid = (top + bottom) // 2
+        if target < matrix[mid][0]:
             bottom = mid - 1
+        elif target > matrix[mid][-1]:
+            top = mid + 1
         else:
             break
+    
+    if not top <= bottom:
+        return False
+
+    row = (top + bottom) // 2
+    left , right = 0 , len(matrix[row]) - 1 
+    while left <= right:
+        mid = (left + right) // 2
+        if target < matrix[row][mid]:
+            right = mid - 1
+        elif target > matrix[row][mid]:
+            left = mid + 1
+        else:
+            return True
+            
+    return False
 
 
 
@@ -27,4 +41,4 @@ matrix = [ [1,3,5,7],
 
 # Output -> Boolean
 
-searchMatrix(matrix, 3)
+searchMatrix(matrix, 0)
